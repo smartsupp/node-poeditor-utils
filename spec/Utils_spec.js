f3e461637dedb7224d297788e17e5bf29ee5064d
Ján Dzurek <famousgarkin@outlook.com>
2015-12-29 09:47:41 +0100

    added get translations utility function

--- spec/Utils_spec.js
+++ spec/Utils_spec.js
@@ -15,17 +15,19 @@ describe('Utils', function () {
 		});
 		expect(Client).toHaveBeenCalledWith('my token');
 	});
+});
+
+describe('Utils', function() {
+	beforeEach(function () {
+		this.utils = new Utils('my token');
+		spyOn(this.utils.client.projects, 'list').and.returnValue(Promise.resolve([
+			{id: 123, name: 'one'},
+			{id: 456, name: 'my project'},
+			{id: 789, name: 'three'}
+		]));
+	});
 
 	describe('#getProject', function () {
-		beforeEach(function () {
-			this.utils = new Utils('my token');
-			spyOn(this.utils.client.projects, 'list').and.returnValue(Promise.resolve([
-				{id: 123, name: 'one'},
-				{id: 456, name: 'my project'},
-				{id: 789, name: 'three'}
-			]));
-		});
-
 		it('returns a promise', function () {
 			expect(this.utils.getProject('my project').then).toEqual(jasmine.any(Function));
 		});
@@ -37,6 +39,36 @@ describe('Utils', function () {
 					id: 456,
 					name: 'my project'
 				}));
+				done();
+			});
+		});
+	});
+
+	describe('#getTranslations', function () {
+		var termsList = jasmine.createSpy('Terms.list').and.callFake(function (languageCode) {
+			return Promise.resolve([1, 2].map(function (i) {
+				return {term: 'app.title.' + i, __languageCode: languageCode, translation: languageCode + ' ' + i};
+			}));
+		});
+		var languagesList = jasmine.createSpy('Languages.list').and.returnValue(Promise.resolve(['en_US', 'de_DE'].map(function (languageCode) {
+			return {
+				terms: {list: function() { return termsList(languageCode); }}
+			};
+		})));
+		var project;
+
+		beforeEach(function () {
+			project = {
+				languages: {list: languagesList}
+			};
+		});
+
+		it('gets all the translations for all the project languages', function (done) {
+			this.utils.getTranslations(project).then(function (translations) {
+				expect(languagesList.calls.count()).toBe(1);
+				expect(termsList.calls.count()).toBe(2);
+				expect(translations.length).toBe(4);
+				expect(translations[0]).toEqual(jasmine.any(Object));
 				done();
 			});
 		});
