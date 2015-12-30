945845827755e02deb194308ad2d9590db54c9aa
Ján Dzurek <famousgarkin@outlook.com>
2015-12-30 07:19:20 +0100

    helper functions separated

--- spec/Utils_spec.js
+++ spec/Utils_spec.js
@@ -1,76 +1,19 @@
 'use strict';
 
-var Promise = require('native-promise-only');
-var rewire = require('rewire');
+var helpers = require('../lib/helpers');
 
-var Utils = rewire('../lib/Utils');
-
-describe('Utils', function () {
-	it('passes API token to poeditor-client', function () {
-		var Client = jasmine.createSpy('Client');
-		Utils.__with__({
-			Client: Client
-		})(function () {
-			new Utils('my token');
-		});
-		expect(Client).toHaveBeenCalledWith('my token');
-	});
-});
+var Utils = require('../lib/Utils');
 
 describe('Utils', function() {
 	beforeEach(function () {
 		this.utils = new Utils('my token');
-		spyOn(this.utils.client.projects, 'list').and.returnValue(Promise.resolve([
-			{id: 123, name: 'one'},
-			{id: 456, name: 'my project'},
-			{id: 789, name: 'three'}
-		]));
 	});
 
 	describe('#getProject', function () {
-		it('returns a promise', function () {
-			expect(this.utils.getProject('my project').then).toEqual(jasmine.any(Function));
-		});
-
-		it('gets project data via API', function (done) {
-			this.utils.getProject('my project')
-			.then(function (project) {
-				expect(project).toEqual(jasmine.objectContaining({
-					id: 456,
-					name: 'my project'
-				}));
-				done();
-			});
-		});
-	});
-
-	describe('#getTranslations', function () {
-		var termsList = jasmine.createSpy('Terms.list').and.callFake(function (languageCode) {
-			return Promise.resolve([1, 2].map(function (i) {
-				return {term: 'app.title.' + i, __languageCode: languageCode, translation: languageCode + ' ' + i};
-			}));
-		});
-		var languagesList = jasmine.createSpy('Languages.list').and.returnValue(Promise.resolve(['en_US', 'de_DE'].map(function (languageCode) {
-			return {
-				terms: {list: function() { return termsList(languageCode); }}
-			};
-		})));
-		var project;
-
-		beforeEach(function () {
-			project = {
-				languages: {list: languagesList}
-			};
-		});
-
-		it('gets all the translations for all the project languages', function (done) {
-			this.utils.getTranslations(project).then(function (translations) {
-				expect(languagesList.calls.count()).toBe(1);
-				expect(termsList.calls.count()).toBe(2);
-				expect(translations.length).toBe(4);
-				expect(translations[0]).toEqual(jasmine.any(Object));
-				done();
-			});
+		it('delegates to helpers.getProject', function () {
+			var helper = spyOn(helpers, 'getProject');
+			this.utils.getProject('my project');
+			expect(helper).toHaveBeenCalledWith('my token', 'my project');
 		});
 	});
 });
---
+++ spec/helpers_spec.js
@@ -0,0 +1,105 @@
+'use strict';
+
+var Client = require('poeditor-client');
+var Promise = require('native-promise-only');
+var rewire = require('rewire');
+
+var helpers = rewire('../lib/helpers');
+
+describe('helpers', function () {
+	describe('#clientFactory', function () {
+		it('creates poeditor-client Client', function () {
+			expect(helpers.clientFactory('my token')).toEqual(jasmine.any(Client));
+		});
+
+		it('passes API token to client', function () {
+			var Client = jasmine.createSpy();
+			helpers.__with__({
+				Client: Client
+			})(function () {
+				helpers.clientFactory('my token');
+			})
+			expect(Client).toHaveBeenCalledWith('my token');
+		});
+	});
+});
+
+describe('helpers', function() {
+	describe('#getProject', function () {
+		beforeEach(function () {
+			var client = {
+				projects: {
+					list: jasmine.createSpy().and.returnValue(Promise.resolve([
+						{id: 123, name: 'one'},
+						{id: 456, name: 'my project'},
+						{id: 789, name: 'three'}
+					]))
+				}
+			};
+			var clientFactory = spyOn(helpers, 'clientFactory').and.returnValue(client);
+			this.restore = helpers.__set__({
+				clientFactory: clientFactory
+			});
+		});
+
+		afterEach(function () {
+			this.restore();
+		});
+
+		it('returns a promise', function () {
+			expect(helpers.getProject('my token', 'my project').then).toEqual(jasmine.any(Function));
+		});
+
+		it('gets project data', function (done) {
+			helpers.getProject('my token', 'my project')
+			.then(function (project) {
+				expect(project).toEqual(jasmine.objectContaining({
+					id: 456,
+					name: 'my project'
+				}));
+				done();
+			})
+			.catch(done.fail);
+		});
+	});
+
+	describe('#getTranslations', function () {
+		beforeEach(function () {
+			var termsList = jasmine.createSpy('Terms.list').and.callFake(function (languageCode) {
+				return Promise.resolve([1, 2].map(function (i) {
+					return {term: 'app.title.' + i, __languageCode: languageCode, translation: languageCode + ' ' + i};
+				}));
+			});
+			this.termsList = termsList;
+			var languagesList = jasmine.createSpy('Languages.list').and.returnValue(Promise.resolve(['en', 'de'].map(function (languageCode) {
+				return {
+					terms: {list: function() { return termsList(languageCode); }}
+				};
+			})));
+			this.languagesList = languagesList;
+			this.project = {
+				languages: {list: languagesList}
+			};
+		});
+
+		it('returns a promise', function () {
+			expect(helpers.getTranslations(this.project).then).toEqual(jasmine.any(Function));
+		});
+
+		it('gets all the translations for all the available project languages', function (done) {
+			helpers.getTranslations(this.project)
+			.then(function (translations) {
+				expect(this.languagesList.calls.count()).toBe(1);
+				expect(this.termsList.calls.count()).toBe(2);
+				expect(translations.length).toBe(4);
+				expect(translations[0]).toEqual(jasmine.objectContaining({
+					term: 'app.title.1',
+					__languageCode: 'en',
+					translation: 'en 1'
+				}));
+				done();
+			}.bind(this))
+			.catch(done.fail);
+		});
+	});
+});
